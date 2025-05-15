4cc1463174e9e66aceec24b5da51992d92d0d0ea
scronheim <scronheim@yandex.ru>
2025-05-15 13:08:51 +0500

    фикс

--- bot/bot.mjs
+++ bot/bot.mjs
@@ -20,7 +20,7 @@ const WEB_URL = 'https://dark-corner.ru/web/index.html#!/server/b7b1b44bf93bed31
 const xhr = axios.create({
   baseURL: API_URL,
   method: 'GET',
-  params: {'X-Plex-Token': process.env.PLEX_TOKEN},
+  params: { 'X-Plex-Token': process.env.PLEX_TOKEN },
   headers: {
     Accept: 'application/json'
   }
@@ -68,22 +68,22 @@ class Bot {
 
   #parseMediaGroup = async (ctx) => {
     for (const message of ctx.mediaGroup) {
-        const fileId = message.audio.file_id
-        const fileName = message.audio.file_name
-        const downloadLink = await this.bot.telegram.getFileLink(fileId)
-        const filepath = downloadLink.pathname
-        const tags = await parseFile(filepath, {skipCovers: true})
-        const albumName = `${tags.common.year} - ${tags.common.album}`
-        const artistName = message.audio.performer
-        let distFilepath = `${DOWNLOAD_DIR_PATH}/${artistName}/${fileName}`
+      const fileId = message.audio.file_id
+      const fileName = message.audio.file_name
+      const downloadLink = await this.bot.telegram.getFileLink(fileId)
+      const filepath = downloadLink.pathname
+      const tags = await parseFile(filepath, { skipCovers: true })
+      const albumName = `${tags.common.year} - ${tags.common.album}`
+      const artistName = message.audio.performer
+      let distFilepath = `${DOWNLOAD_DIR_PATH}/${artistName}/${fileName}`
 
-        if (albumName) {
-          distFilepath = `${MUSIC_PATH}/${artistName}/${albumName}/${fileName}`
-        }
-        
-        this.#checkArtistExist(artistName)
-        await cp(filepath, distFilepath)
-        await unlink(filepath)
+      if (albumName) {
+        distFilepath = `${MUSIC_PATH}/${artistName}/${albumName}/${fileName}`
+      }
+
+      this.#checkArtistExist(artistName)
+      await cp(filepath, distFilepath)
+      await unlink(filepath)
     }
     await this.#downloadComplete(ctx)
   }
@@ -91,25 +91,25 @@ class Bot {
   #extractArtistName = (filename) => {
     // Удаляем расширение файла
     const baseName = filename.replace(/\.[^/.]+$/, '');
-    
+
     // Проверяем распространенные разделители с пробелами и тире
     const commonSeparators = [/\s-\s/, /\s–\s/, /\s—\s/];
     for (const sep of commonSeparators) {
-        const splitIndex = baseName.search(sep);
-        if (splitIndex !== -1) {
-            return baseName.substring(0, splitIndex).trim();
-        }
+      const splitIndex = baseName.search(sep);
+      if (splitIndex !== -1) {
+        return baseName.substring(0, splitIndex).trim();
+      }
     }
-    
+
     // Проверяем другие разделители: "_", "(", "[", пробел и тире
     const otherSeparators = [/_/, /\(/, /\[/, /\s-\s?/, /-/];
     for (const sep of otherSeparators) {
-        const splitIndex = baseName.search(sep);
-        if (splitIndex !== -1) {
-            return baseName.substring(0, splitIndex).trim();
-        }
+      const splitIndex = baseName.search(sep);
+      if (splitIndex !== -1) {
+        return baseName.substring(0, splitIndex).trim();
+      }
     }
-    
+
     // Если разделителей нет, возвращаем исходное имя
     return baseName.trim();
   }
@@ -147,12 +147,12 @@ class Bot {
           genre: a.Genre.map(g => g.tag),
           url: `${WEB_URL}/details?key=/library/metadata/${a.ratingKey}`,
           type: a.title.includes('EP') ? ALBUM_TYPES.EP
-           : (a.title.includes('Single')) ? ALBUM_TYPES.Single
-           : (a.title.includes('Demo')) ? ALBUM_TYPES.Demo 
-           : (a.title.includes('Live')) ? ALBUM_TYPES.Live
-           : (a.title.includes('Instrument')) ? ALBUM_TYPES.Instrumental
-           : (a.title.includes('Remix')) ? ALBUM_TYPES.Remixes
-           : ALBUM_TYPES['Full-Lenght']
+            : (a.title.includes('Single')) ? ALBUM_TYPES.Single
+              : (a.title.includes('Demo')) ? ALBUM_TYPES.Demo
+                : (a.title.includes('Live')) ? ALBUM_TYPES.Live
+                  : (a.title.includes('Instrument')) ? ALBUM_TYPES.Instrumental
+                    : (a.title.includes('Remix')) ? ALBUM_TYPES.Remixes
+                      : ALBUM_TYPES['Full-Lenght']
         }
       })
     }
@@ -188,7 +188,7 @@ class Bot {
   }
 
   #checkArtistExist = (artist) => {
-     if (!fs.existsSync(`${MUSIC_PATH}/${artist}`)) fs.mkdirSync(`${MUSIC_PATH}/${artist}`)
+    if (!fs.existsSync(`${MUSIC_PATH}/${artist}`)) fs.mkdirSync(`${MUSIC_PATH}/${artist}`)
   }
 
   #downloadFile = async (artist, downloadUrl) => {
@@ -233,7 +233,7 @@ class Bot {
             filepath,
             targetPath: outputPath
           });
-      
+
           [...extractor.extract().files]
           this.#unpackComplete(ctx, filepath, outputPath)
         } catch (err) {
@@ -272,70 +272,72 @@ class Bot {
     const instrumentalAlbums = discography.albums.filter(a => a.type === ALBUM_TYPES.Instrumental)
 
     let caption =
-`
+      `
 <a href="${discography.artistUrl}">${discography.artist}</a> - ${discography.artistGenre.join(' / ')} (${discography.country})
 `
-    if (fullLenghtAlbums.length) caption += 
-`
-Полноформатники: ${fullLenghtAlbums.map((a, index) => 
-`
+    if (fullLenghtAlbums.length) caption +=
+      `
+Полноформатники: ${fullLenghtAlbums.map((a, index) =>
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
     if (epAlbums.length) caption +=
-`
+      `
 EP: ${epAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
     if (singlesAlbums.length) caption +=
-`
+      `
 Синглы: ${singlesAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
 
     if (demoAlbums.length) caption +=
-`
+      `
 Демо: ${demoAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
 
     if (liveAlbums.length) caption +=
-`
+      `
 Live: ${liveAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
 
     if (remixesAlbums.length) caption +=
-`
+      `
 Ремиксы: ${remixesAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
 
     if (instrumentalAlbums.length) caption +=
-`
+      `
 Инструментал: ${instrumentalAlbums.map((a, index) =>
-`
+        `
 ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
 `
     caption +=
-`
+      `
 #discography`
-    ctx.telegram.sendPhoto('@dark_corner_ru', {url: discography.artistThumbUrl}, {caption, parse_mode: 'HTML'}) // 423754317   @dark_corner_ru
+    ctx.telegram.sendPhoto('@dark_corner_ru', { url: discography.artistThumbUrl }, { caption, parse_mode: 'HTML' }) // 423754317   @dark_corner_ru
   }
 
   #postAlbumToChannel = async (ctx, albumInfo) => {
-    ctx.telegram.sendPhoto('423754317', {url: albumInfo.coverUrl}, {caption: // 423754317   @dark_corner_ru
-`
+    ctx.telegram.sendPhoto('423754317', { url: albumInfo.coverUrl }, {
+      caption: // 423754317   @dark_corner_ru
+        `
 <a href="${albumInfo.artistUrl}">${albumInfo.artist}</a> - <a href="${albumInfo.albumUrl}">${albumInfo.album}</a> (${albumInfo.year})
 
 Жанр(ы): ${albumInfo.genres.map(g => g.tag).join(' / ')}
 Страна: ${albumInfo.artistCountry}
 `,
-    parse_mode: 'HTML'})
+      parse_mode: 'HTML'
+    })
   }
 
   #sanitizeText = (text) => {
@@ -349,10 +351,10 @@ ${index + 1}. <a href="${a.url}">${a.title}</a> (${a.year})`).join('')}
   }
 
   #listDirectories = async (pth) => {
-    const directories = (await readdir(pth, {withFileTypes: true}))
+    const directories = (await readdir(pth, { withFileTypes: true }))
       .filter(dirent => dirent.isDirectory())
       .map(dir => dir.name)
-  
+
     return directories
   }
 }
